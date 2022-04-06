9ea82ba59c5449a74bb1c4a56c40ef8f729a3603
Oshan12345 <bsagor99@gmail.com>
2022-04-06 17:51:39 +0600

    practicing graph data structure

--- dataStructures/graph/graph.js
+++ dataStructures/graph/graph.js
@@ -38,22 +38,100 @@ class Graph {
       }
     }
   }
+
+  toString() {
+    let str = "";
+
+    this.#adjacentList.forEach((val, key) => {
+      str += `${key} -> [  ${Array.from(val).join(", ")}  ]\n`;
+    });
+    return str;
+  }
 }
 
 const graph = new Graph();
 
+// printing all the alphabet
+const alphabet = Array.from({ length: 26 }, (_, i) =>
+  String.fromCharCode(i + 65)
+);
+
+const randomIndex = (max, min = 0) => {
+  return Math.floor(Math.random() * Math.floor(max) + Math.floor(min));
+};
+
+// alphabet.forEach((letter) =>
+//   graph.addEdge(letter, alphabet[randomIndex(alphabet.length - 1)])
+// );
+
 const vertices = ["a", "b", "c", "d", "e", "f"];
+
+// graph.addEdge("a", "b");
+// graph.addEdge("a", "c");
+// graph.addEdge("a", "d");
+// graph.addEdge("b", "e");
+// graph.addEdge("b", "f");
+
+// {
+//   a: [ 'b', 'c', 'd' ],
+//   b: [ 'a', 'e', 'f', 'c', 'd' ],
+//   c: [ 'a', 'b' ],
+//   d: [ 'a', 'b' ],
+//   e: [ 'b' ],
+//   f: [ 'b' ]
+// }
+
+// console.log(graph.vertices);
+// console.log(graph.toString());
+
+const COLORS = Object.freeze({
+  GREEN: "green",
+  YELLOW: "yellow",
+  RED: "red",
+});
+
+// it follow the queue
+const breathFirstSearch = (graph, fromVertex, callback) => {
+  const { vertices, adjacentList } = graph;
+
+  if (vertices.length === 0) {
+    return;
+  }
+
+  const color = vertices.reduce((colors, vertex) => {
+    return { ...colors, [vertex]: COLORS.GREEN };
+  }, {});
+
+  const queue = [];
+
+  queue.push(fromVertex);
+
+  while (queue.length > 0) {
+    const vertex = queue.shift();
+
+    const nearVertex = adjacentList[vertex];
+    color[vertex] = COLORS.YELLOW;
+    callback(color);
+    nearVertex.forEach((nVertex) => {
+      if (color[nVertex] === COLORS.GREEN) {
+        color[nVertex] = COLORS.YELLOW;
+        queue.push(nVertex);
+      }
+    });
+
+    color[vertex] = COLORS.RED;
+    callback(color);
+    callback(vertex);
+  }
+};
 
 graph.addEdge("a", "b");
 graph.addEdge("a", "c");
 graph.addEdge("a", "d");
 graph.addEdge("b", "e");
 graph.addEdge("b", "f");
-graph.addEdge("b", "b");
-graph.addEdge("c", "b");
-graph.addEdge("c", "b");
-graph.addEdge("c", "b");
-graph.addEdge("d", "b");
 
-console.log(graph.vertices);
-console.log(graph.adjacentList);
+console.log(graph.toString());
+breathFirstSearch(graph, "a", console.log);
+
+// [a,b,c,d,e,f]
